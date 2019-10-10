071e000617cc95678c3ecf6758ecac9258e4ae20
whitehox <oketegah@gmail.com>
2019-10-10 15:37:33 +0100

    completed shoutout form

---
+++ src/Components/ShoutOutForm/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import InputField from '../InputField';
+
+class ShoutOutForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      location: '',
+      image: '',
+      videoUrl: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <>
+        <form id="shoutoutForm">
+          <p>Fill to Create Shout Out</p>
+          <InputField
+            type="text"
+            id="name"
+            name="name"
+            onchange={this.handleChange.bind(this)}
+            placeholder="Display Name"
+            value={this.state.name}
+          />
+          <InputField
+            type="text"
+            id="location"
+            name="location"
+            placeholder="Your Location"
+            onchange={this.handleChange.bind(this)}
+            value={this.state.location}
+          />
+          <InputField
+            type="file"
+            id="image"
+            name="image"
+            value={this.state.image}
+          />
+
+          <InputField
+            type="text"
+            id="videoUrl"
+            name="videoUrl"
+            onchange={this.handleChange.bind(this)}
+            placeholder="Video link if any"
+            value={this.state.videoUrl}
+          />
+          <InputField
+            type="submit"
+            id="submitSo"
+            name="submitSo"
+            onchange={this.handleChange}
+            value="Push da SO"
+            onclick={this.handleFormSubmit.bind(this)}
+          />
+        </form>
+      </>
+    );
+  }
+}
+
+export default ShoutOutForm;
