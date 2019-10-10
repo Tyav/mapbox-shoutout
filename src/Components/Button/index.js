6632271ebf1dab8e3f7fbea60261cc3b1f92e15e
whitehox <oketegah@gmail.com>
2019-10-10 15:36:33 +0100

    added icons for button component

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -4,10 +4,13 @@ class Button extends Component {
   render() {
     return (
       <>
-        <button style={this.props.style} onClick={this.props.onclick}>
+        <button style={this.props.styles} onClick={this.props.onclick}>
+          <i className={`mdi mdi-${this.props.icon}`}></i>
           {this.props.text}
         </button>
       </>
     );
   }
 }
+
+export default Button;
